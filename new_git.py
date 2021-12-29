a59ebddb93d72fdbd19e608b0ae810f1fce316a1
Taras <yand3x.ambassador@yandex.ru>
2021-12-30 01:20:49 +0500

    hero animation(pre-release version)

--- new_git.py
+++ new_git.py
@@ -39,8 +39,10 @@ hero_images_walk = [load_image('image_part_001.png'), load_image('image_part_002
                     load_image('image_part_024.png'), load_image('image_part_025.png'),
                     load_image('image_part_026.png'), load_image('image_part_027.png')]
 
-WALK_RIGHT = True
-
+WALK_RIGHT = False
+WALK_LEFT = False
+JUMP = False
+jump_size = 8
 
 all_sprites = pygame.sprite.Group()
 hero = pygame.sprite.Sprite()
@@ -60,18 +62,40 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] and hero.rect.left > 0:
-            hero.rect.left -= hero_speed
-            hero.image = hero_images_walk[hod_count]
-            hod_count = (hod_count + 1) % 26
-            print(hod_count)
+        if event.type == pygame.KEYDOWN:
+            if pygame.key.get_pressed()[pygame.K_RIGHT]:
+                WALK_RIGHT = True
+                WALK_LEFT = False
+            if pygame.key.get_pressed()[pygame.K_LEFT]:
+                WALK_RIGHT = False
+                WALK_LEFT = True
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                JUMP = True
+        if event.type == pygame.KEYUP:
+            if pygame.K_UP:
+                WALK_RIGHT = False
+                WALK_LEFT = False
 
-        if key[pygame.K_RIGHT] and hero.rect.right < 1920:
-            hero.rect.left += hero_speed
-            hero.image = hero_images_walk[hod_count]
-            hod_count = (hod_count + 1) % 26
-            print(hod_count)
+    if not WALK_RIGHT and not WALK_LEFT:
+        hero.image = hero_image
+
+    elif WALK_LEFT and hero.rect.left > 0:
+        hero.rect.left -= hero_speed
+        hero.image = pygame.transform.flip(hero_images_walk[hod_count], True, False)
+        hod_count = (hod_count + 1) % 26
+
+    elif WALK_RIGHT and hero.rect.right < 1920:
+        hero.rect.left += hero_speed
+        hero.image = hero_images_walk[hod_count]
+        hod_count = (hod_count + 1) % 26
+
+    if JUMP:
+        if jump_size >= -8:
+            hero.rect.bottom -= jump_size * abs(jump_size)
+            jump_size -= 1
+        else:
+            jump_size = 8
+            JUMP = False
 
     screen.blit(background_image, (0, 0))
     all_sprites.draw(screen)
