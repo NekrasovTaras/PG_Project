e5e5df5728dd86d5d99fa3e0f97d242fbd61ec5a
Taras <yand3x.ambassador@yandex.ru>
2022-01-03 02:29:07 +0500

    hero animation(Final version) + collides(v.0.01) + individual sprite classes

--- new_git.py
+++ new_git.py
@@ -23,39 +23,113 @@ def load_image(name, color_key=None):
     return image
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.hero_image = load_image('image_part_022.png')
+        self.hero_images_walk = [load_image('image_part_001.png'), load_image('image_part_002.png'),
+                            load_image('image_part_003.png'), load_image('image_part_004.png'),
+                            load_image('image_part_005.png'), load_image('image_part_006.png'),
+                            load_image('image_part_007.png'), load_image('image_part_008.png'),
+                            load_image('image_part_009.png'), load_image('image_part_010.png'),
+                            load_image('image_part_011.png'), load_image('image_part_012.png'),
+                            load_image('image_part_013.png'), load_image('image_part_014.png'),
+                            load_image('image_part_015.png'), load_image('image_part_016.png'),
+                            load_image('image_part_017.png'), load_image('image_part_018.png'),
+                            load_image('image_part_019.png'), load_image('image_part_020.png'),
+                            load_image('image_part_021.png'), load_image('image_part_023.png'),
+                            load_image('image_part_024.png'), load_image('image_part_025.png'),
+                            load_image('image_part_026.png'), load_image('image_part_027.png')]
+        
+        self.WALK_RIGHT = False
+        self.WALK_LEFT = False
+        self.JUMP = False
+        self.jump_size = 8
+
+        self.hero = pygame.sprite.Sprite()
+        self.hero.image = self.hero_image
+        self.hero.rect = self.hero.image.get_rect()
+        self.hero.rect.left = 0
+        self.hero.rect.bottom = 1080
+        self.hod_count = 0
+        self.hero_speed = 10
+        all_sprites.add(self.hero)
+        
+    def Walk_Left(self):
+        self.WALK_RIGHT = False
+        self.WALK_LEFT = True
+        
+    def Walk_Right(self):
+        self.WALK_RIGHT = True
+        self.WALK_LEFT = False
+
+    def Stop_Walk_Left(self):
+        self.WALK_LEFT = False
+
+    def Stop_Walk_Right(self):
+        self.WALK_RIGHT = False
+
+    def Jump(self):
+        self.JUMP = True
+
+    def Update(self):
+        if not self.WALK_RIGHT and not self.WALK_LEFT:
+            self.hero.image = self.hero_image
+
+        elif self.WALK_LEFT and self.hero.rect.left > 0:
+            self.hero.rect.left -= self.hero_speed
+            self.hero.image = pygame.transform.flip(self.hero_images_walk[self.hod_count], True, False)
+            self.hod_count = (self.hod_count + 1) % 26
+
+        elif self.WALK_RIGHT and self.hero.rect.right < 1920:
+            self.hero.rect.left += self.hero_speed
+            self.hero.image = self.hero_images_walk[self.hod_count]
+            self.hod_count = (self.hod_count + 1) % 26
+
+        if self.JUMP:
+            if self.jump_size >= -8:
+                self.hero.rect.bottom -= self.jump_size * abs(self.jump_size)
+                self.jump_size -= 1
+            else:
+                self.jump_size = 8
+                self.JUMP = False
+
+        collides = pygame.sprite.spritecollide(self.hero, level_1.platformi, False)
+        for plat in collides:
+            if self.hero.rect.bottom >= plat.rect.top:
+                self.hero.rect.bottom = plat.rect.top
+
+        if not collides and self.hero.rect.bottom < 1080 and not self.JUMP:
+            self.hero.rect.bottom += 5
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = load_image('platform_fly.png')
+        self.rect = self.image.get_rect()
+
+
+class Level_1(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.platformi = pygame.sprite.Group()
+        layers_on_level = [[1300, 800], [200, 400]]
+        for platform in layers_on_level:
+            block = Platform()
+            block.rect.x = platform[0]
+            block.rect.y = platform[1]
+            self.platformi.add(block)
+        all_sprites.add(self.platformi)
+
+
 background_image = load_image('background.jpg')
-hero_image = load_image('image_part_022.png')
-hero_images_walk = [load_image('image_part_001.png'), load_image('image_part_002.png'),
-                    load_image('image_part_003.png'), load_image('image_part_004.png'),
-                    load_image('image_part_005.png'), load_image('image_part_006.png'),
-                    load_image('image_part_007.png'), load_image('image_part_008.png'),
-                    load_image('image_part_009.png'), load_image('image_part_010.png'),
-                    load_image('image_part_011.png'), load_image('image_part_012.png'),
-                    load_image('image_part_013.png'), load_image('image_part_014.png'),
-                    load_image('image_part_015.png'), load_image('image_part_016.png'),
-                    load_image('image_part_017.png'), load_image('image_part_018.png'),
-                    load_image('image_part_019.png'), load_image('image_part_020.png'),
-                    load_image('image_part_021.png'), load_image('image_part_023.png'),
-                    load_image('image_part_024.png'), load_image('image_part_025.png'),
-                    load_image('image_part_026.png'), load_image('image_part_027.png')]
-
-WALK_RIGHT = False
-WALK_LEFT = False
-JUMP = False
-jump_size = 8
-
 all_sprites = pygame.sprite.Group()
-hero = pygame.sprite.Sprite()
-hero.image = hero_image
-hero.rect = hero.image.get_rect()
-hero.rect.left = 0
-hero.rect.bottom = 1080
-all_sprites.add(hero)
-
-hod_count = 0
-hero_speed = 10
+level_1 = Level_1()
+Hero = Player()
 FPS = 50
 clock = pygame.time.Clock()
+
 
 running = True
 while running:
@@ -64,39 +138,17 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                WALK_RIGHT = True
-                WALK_LEFT = False
+                Hero.Walk_Right()
             if pygame.key.get_pressed()[pygame.K_LEFT]:
-                WALK_RIGHT = False
-                WALK_LEFT = True
+                Hero.Walk_Left()
             if pygame.key.get_pressed()[pygame.K_SPACE]:
-                JUMP = True
+                Hero.Jump()
         if event.type == pygame.KEYUP:
-            if pygame.K_UP:
-                WALK_RIGHT = False
-                WALK_LEFT = False
-
-    if not WALK_RIGHT and not WALK_LEFT:
-        hero.image = hero_image
-
-    elif WALK_LEFT and hero.rect.left > 0:
-        hero.rect.left -= hero_speed
-        hero.image = pygame.transform.flip(hero_images_walk[hod_count], True, False)
-        hod_count = (hod_count + 1) % 26
-
-    elif WALK_RIGHT and hero.rect.right < 1920:
-        hero.rect.left += hero_speed
-        hero.image = hero_images_walk[hod_count]
-        hod_count = (hod_count + 1) % 26
-
-    if JUMP:
-        if jump_size >= -8:
-            hero.rect.bottom -= jump_size * abs(jump_size)
-            jump_size -= 1
-        else:
-            jump_size = 8
-            JUMP = False
-
+            if event.key == pygame.K_LEFT:
+                Hero.Stop_Walk_Left()
+            if event.key == pygame.K_RIGHT:
+                Hero.Stop_Walk_Right()
+    Hero.Update()
     screen.blit(background_image, (0, 0))
     all_sprites.draw(screen)
     pygame.display.flip()
