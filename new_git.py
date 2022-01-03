aaf688221ae7f0b9511fcd9454160b93eaf61191
Taras <yand3x.ambassador@yandex.ru>
2022-01-03 18:56:24 +0500

    bug fix + 1280x720 resolution

--- new_git.py
+++ new_git.py
@@ -3,7 +3,7 @@ import pygame
 import sys
 
 pygame.init()
-size = width, height = 1920, 1080
+size = width, height = 1280, 720
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Epic adventure')
 
@@ -11,7 +11,7 @@ pygame.display.set_caption('Epic adventure')
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
-        print('Файла не существует')
+        print('Файла не существует', fullname)
         sys.exit()
     image = pygame.image.load(fullname)
     if color_key is not None:
@@ -28,37 +28,38 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.hero_image = load_image('image_part_022.png')
         self.hero_images_walk = [load_image('image_part_001.png'), load_image('image_part_002.png'),
-                            load_image('image_part_003.png'), load_image('image_part_004.png'),
-                            load_image('image_part_005.png'), load_image('image_part_006.png'),
-                            load_image('image_part_007.png'), load_image('image_part_008.png'),
-                            load_image('image_part_009.png'), load_image('image_part_010.png'),
-                            load_image('image_part_011.png'), load_image('image_part_012.png'),
-                            load_image('image_part_013.png'), load_image('image_part_014.png'),
-                            load_image('image_part_015.png'), load_image('image_part_016.png'),
-                            load_image('image_part_017.png'), load_image('image_part_018.png'),
-                            load_image('image_part_019.png'), load_image('image_part_020.png'),
-                            load_image('image_part_021.png'), load_image('image_part_023.png'),
-                            load_image('image_part_024.png'), load_image('image_part_025.png'),
-                            load_image('image_part_026.png'), load_image('image_part_027.png')]
-        
+                                 load_image('image_part_003.png'), load_image('image_part_004.png'),
+                                 load_image('image_part_005.png'), load_image('image_part_006.png'),
+                                 load_image('image_part_007.png'), load_image('image_part_008.png'),
+                                 load_image('image_part_009.png'), load_image('image_part_010.png'),
+                                 load_image('image_part_011.png'), load_image('image_part_012.png'),
+                                 load_image('image_part_013.png'), load_image('image_part_014.png'),
+                                 load_image('image_part_015.png'), load_image('image_part_016.png'),
+                                 load_image('image_part_017.png'), load_image('image_part_018.png'),
+                                 load_image('image_part_019.png'), load_image('image_part_020.png'),
+                                 load_image('image_part_021.png'), load_image('image_part_023.png'),
+                                 load_image('image_part_024.png'), load_image('image_part_025.png'),
+                                 load_image('image_part_026.png'), load_image('image_part_027.png')]
+
         self.WALK_RIGHT = False
         self.WALK_LEFT = False
         self.JUMP = False
         self.jump_size = 8
+        self.fall_size = 1
 
         self.hero = pygame.sprite.Sprite()
         self.hero.image = self.hero_image
         self.hero.rect = self.hero.image.get_rect()
         self.hero.rect.left = 0
-        self.hero.rect.bottom = 1080
+        self.hero.rect.bottom = 720
         self.hod_count = 0
         self.hero_speed = 10
         all_sprites.add(self.hero)
-        
+
     def Walk_Left(self):
         self.WALK_RIGHT = False
         self.WALK_LEFT = True
-        
+
     def Walk_Right(self):
         self.WALK_RIGHT = True
         self.WALK_LEFT = False
@@ -81,12 +82,12 @@ class Player(pygame.sprite.Sprite):
             self.hero.image = pygame.transform.flip(self.hero_images_walk[self.hod_count], True, False)
             self.hod_count = (self.hod_count + 1) % 26
 
-        elif self.WALK_RIGHT and self.hero.rect.right < 1920:
+        elif self.WALK_RIGHT and self.hero.rect.right < 1280:
             self.hero.rect.left += self.hero_speed
             self.hero.image = self.hero_images_walk[self.hod_count]
             self.hod_count = (self.hod_count + 1) % 26
 
-        if self.JUMP:
+        if self.JUMP and self.fall_size == 1:
             if self.jump_size >= -8:
                 self.hero.rect.bottom -= self.jump_size * abs(self.jump_size)
                 self.jump_size -= 1
@@ -96,11 +97,14 @@ class Player(pygame.sprite.Sprite):
 
         collides = pygame.sprite.spritecollide(self.hero, level_1.platformi, False)
         for plat in collides:
-            if self.hero.rect.bottom >= plat.rect.top:
-                self.hero.rect.bottom = plat.rect.top
+            if self.hero.rect.bottom > plat.rect.top:
+                self.hero.rect.bottom = plat.rect.top + 1
 
-        if not collides and self.hero.rect.bottom < 1080 and not self.JUMP:
-            self.hero.rect.bottom += 5
+        if not collides and self.hero.rect.bottom < 720 and not self.JUMP:
+            self.hero.rect.bottom += self.fall_size
+            self.fall_size += 1
+        else:
+            self.fall_size = 1
 
 
 class Platform(pygame.sprite.Sprite):
@@ -114,7 +118,7 @@ class Level_1(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.platformi = pygame.sprite.Group()
-        layers_on_level = [[1300, 800], [200, 400]]
+        layers_on_level = [[500, 100], [200, 400]]
         for platform in layers_on_level:
             block = Platform()
             block.rect.x = platform[0]
@@ -123,13 +127,12 @@ class Level_1(pygame.sprite.Sprite):
         all_sprites.add(self.platformi)
 
 
-background_image = load_image('background.jpg')
+background_image = load_image('background.png')
 all_sprites = pygame.sprite.Group()
 level_1 = Level_1()
 Hero = Player()
-FPS = 50
+FPS = 60
 clock = pygame.time.Clock()
-
 
 running = True
 while running:
@@ -152,6 +155,6 @@ while running:
     screen.blit(background_image, (0, 0))
     all_sprites.draw(screen)
     pygame.display.flip()
-    clock.tick(50)
+    clock.tick(FPS)
 
 pygame.quit()
